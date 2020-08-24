212839e5346210bea1774d1fd1607d3d123fe2d4
yumikaseyumikase <1673fdnj@gmail.com>
2020-08-25 00:31:51 +0900

    kouda_0819から上限値用migrateとtime limitも

---
+++ database/migrations/2020_08_25_002504_add_limit_to_matters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLimitToMattersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matters', function (Blueprint $table) {
+            $table->integer('barning_limit');
+            $table->integer('progress_limit');
+            $table->integer('time_limit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matters', function (Blueprint $table) {
+            //
+        });
+    }
+}
