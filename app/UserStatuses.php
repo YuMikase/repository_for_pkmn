dac5bb65efaae861e36d8450f857aefe81ce8d92
yumikaseyumikase <1673fdnj@gmail.com>
2020-08-29 16:23:40 +0900

    ユーザーのステータス追加

---
+++ app/UserStatuses.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserStatuses extends Model
+{
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2020_08_29_161448_create_user_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type');
+            $table->integer('value1');
+            $table->integer('value2');
+            $table->integer('value3');
+            $table->integer('value4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_statuses');
+    }
+}
