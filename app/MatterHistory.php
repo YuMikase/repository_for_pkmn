b750789d52fa97c33195140973c2b3ce09b9bf43
dmjdesu <dmjdesu@gmail.com>
2020-09-06 15:50:35 +0900

    終了処理関連追加

--- app/MatterHistory.php
+++ app/MatterHistory.php
@@ -8,4 +8,13 @@ class MatterHistory extends Model
 {
     protected $guarded = ['id'];
     protected $table = 'matters_history';
+
+    /**
+     * 戦闘履歴に所属するユーザーを取得
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+
 }
