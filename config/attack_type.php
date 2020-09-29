d5a47d2fa1e5768ea9ed521c6b0a5034d718f98e
ost-s-koda <s-koda@ostechnology.co.jp>
2020-09-29 10:44:41 +0900

    atack_typeの設定値変更

--- config/attack_type.php
+++ config/attack_type.php
@@ -12,8 +12,8 @@ return [
             ],
             
             5 => [
-                'barning' => 20,
-                'progress' => -20,
+                'barning' => 10,
+                'progress' => -10,
                 'message' => '案件からの攻撃！！',
             ]
         ],
@@ -40,18 +40,18 @@ return [
         'id' => 201,
         'terms' => [
             1 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「先輩、お疲れ様でした！」「？まだ定時だよ」',
             ],
             2 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「おっ、今日はノー残業デーだな。タイムカード打刻してきていいよ」「わかりました！」',
             ],
             3 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「じゃあその後この業務今日中でお願いね」',
             ],
         ],
@@ -61,18 +61,18 @@ return [
         'id' => 202,
         'terms' => [
             1 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「このタスク明日中でお願いね」「わかりました！」',
             ],
             5 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「このタスク今日中って言ったよね？」「えっ、いや明日って」',
             ],
             6 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「うるさい！言い訳するな！」',
             ],
         ],
@@ -82,13 +82,13 @@ return [
         'id' => 203,
         'terms' => [
             1 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 8,
+                'progress' => -8,
                 'message' => '「よし、このタスクのやり方を先輩に聞くぞ！」',
             ],
             3 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 8,
+                'progress' => -8,
                 'message' => '「あぁ君、先輩君は今倒れて入院しているから、携帯に電話してあげて」',
             ],
         ],
@@ -98,39 +98,39 @@ return [
         'id' => 204,
         'terms' => [
             1 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「あの、有休を使いたいんですけど」',
             ],
             2 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「？ごめん、よく聞き取れなかった。もっかい言って」',
             ],
             3 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「あの、ゆうきゅう」「？ごめん、もっかい」',
             ],
             6 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「そういえば以前依頼されていたタスク今日中で終われそうです」',
             ],
             7 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「そっか、了解」',
             ],
             8 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 3,
+                'progress' => -3,
                 'message' => '「それで、あの、ゆうきゅ」',
             ],
             9 => [
-                'barning' => 1,
-                'progress' => -1,
-                'message' => '「ごめん、電話機の調子悪いから一回切るね！」',
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「ごめん、電話機の調子悪いから一回切る！」',
             ],
         ],
     ],
@@ -139,13 +139,13 @@ return [
         'id' => 205,
         'terms' => [
             4 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 8,
+                'progress' => -8,
                 'message' => '「プログラム修正したのでレビューお願いします。」',
             ],
             6 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 8,
+                'progress' => -8,
                 'message' => '「おう。印刷して持ってこい。」',
             ],
         ],
@@ -155,26 +155,136 @@ return [
         'id' => 206,
         'terms' => [
             4 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「テスト終わりました！バグは0件です！」',
             ],
             5 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「嘘つけ、出ないわけないだろ。もっかいテストしてこい」',
             ],
             6 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「いやでも、もう何回も・・・・」',
             ],
             7 => [
-                'barning' => 1,
-                'progress' => -1,
+                'barning' => 5,
+                'progress' => -5,
                 'message' => '「出ないならバグ適当に作ってこい」',
             ],
         ],
     ],
+
+    207 => [
+        'id' => 207,
+        'terms' => [
+            1 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「あの、すいません」',
+            ],
+            2 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「？どうした」',
+            ],
+            3 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「僕だけ担当テストが他の人の10倍あるんですが・・」',
+            ],
+            4 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「そうか」',
+            ],
+            5 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「いや、さすがにスケジュール的に無理が・・」',
+            ],
+            6 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「そうか」',
+            ],
+            7 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「・・・頑張ります」',
+            ],
+            8 => [
+                'barning' => 2,
+                'progress' => -2,
+                'message' => '「そうか」',
+            ],
+        ],
+    ],
+
+    208 => [
+        'id' => 208,
+        'terms' => [
+            4 => [
+                'barning' => 5,
+                'progress' => -5,
+                'message' => '「部長！プロジェクトが炎上していて手が付けられません！」',
+            ],
+            5 => [
+                'barning' => 5,
+                'progress' => -5,
+                'message' => '「なに！？あれだけ余裕のあるスケジュールだったのにどういうことだ！？」',
+            ],
+            6 => [
+                'barning' => 5,
+                'progress' => -5,
+                'message' => '「いやだって、部長が客先の要望を全部引き受けるから工数が増えに増えて・・」',
+            ],
+            7 => [
+                'barning' => 5,
+                'progress' => -5,
+                'message' => '「それを何とかするのが君らの仕事だろ！さっさとやれ！」',
+            ],
+        ],
+    ],
+
+    209 => [
+        'id' => 209,
+        'terms' => [
+            4 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「部長！Aさんが過労で倒れてしまいました！」',
+            ],
+            5 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「バケツで水をかけとけ！エンジニアなら起きるはずだ！」',
+            ],
+            6 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「部長！かけましたが反応がないです！」',
+            ],
+            7 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「だったら耳元で叫べ！起きるはずだ！さっさとしごとさせろ！」',
+            ],
+            8 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「部長！叫びましたが動きません！あとなんかぶつぶつ半笑いで言ってます！」',
+            ],
+            9 => [
+                'barning' => 3,
+                'progress' => -3,
+                'message' => '「笑っているなら業務が出来るはずだ！さっさとやらせろ！」',
+            ],
+        ],
+    ],
+
+
 
 ];
