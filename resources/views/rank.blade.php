bbedc7edf1ef9d3a576fe453f77f02d19cd266f7
ost-s-koda <s-koda@ostechnology.co.jp>
2020-10-01 14:45:46 +0900

    フッター削除

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -10,11 +10,6 @@
     <hr style="border:0;border-top:1px solid red;">
   @endforeach
 </div>
-<footer class="footer">
-  <div class="container">
-    <button class="col m-1 btn  btn-primary" type="button" name="button" onclick="location.href='/'">案件を退出する。</button>
-  </div>
-</footer>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
