ddd2ac83e14dc2cd979941fb9bfc93c085f929eb
ost-s-koda <s-koda@ostechnology.co.jp>
2020-10-02 11:35:53 +0900

    一旦push

--- database/migrations/2020_08_02_164734_create_matters_table.php
+++ database/migrations/2020_08_02_164734_create_matters_table.php
@@ -16,7 +16,7 @@ class CreateMattersTable extends Migration
         Schema::create('matters', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('skill_count');
-            $table->integer('barning');
+            $table->integer('barning')->unsigned();
             $table->integer('progress');
             $table->integer('time');
             $table->timestamps();
